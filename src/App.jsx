dc366414ad535c4e630275da9e612b8b9291316c
Alfredo20207 <alfredoresendiz39@gmail.com>
2026-07-07 13:11:47 -0600

    react is not definied
    sse corrige error de react is not defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react'
 import Papa from 'papaparse';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianGrid } from 'recharts';
 
